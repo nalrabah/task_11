0d5ec14e88d03126dfc48f15a5b1046926f8031b
nalrabah <nalrabah@hotmail.com>
2019-01-31 19:52:47 +0300

    done

--- task_11/urls.py
+++ task_11/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('signup/',views.signup ,name='signup'),
     path('signin/',views.signin ,name='signin'),
     path('signout/',views.signout ,name='signout'),
+    path('noacess/',views.noaccess ,name='no-access')
 ]
 
 if settings.DEBUG:
